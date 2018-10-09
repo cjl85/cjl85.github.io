2e37a6a50991b6f3a587faf07cad0ad448f5256d
CL <cl@CLs-MacBook-Pro.local>
2018-10-09 02:14:51 -0600

    add value, suit, point

--- BlackJack/apps.js
+++ BlackJack/apps.js
@@ -1,18 +1,23 @@
-
 const newDeck1 = [];
 
 const suits = ["♦", "♣", "♥", "♠"];
 const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
 // console.log(Object.entries(deck));
 // console.log(deck);
+// console.log(points);
+for (const suit of suits) {
+  for (const value of values) {
+    // console.log(suit);
+    // console.log(value);
+    // console.log(card);
 
-for (let suit of suits) {
-  for(let value of values) {
-    console.log(suit);
-    console.log(value);
 
-    // newDeck1.push(card);
-    // console.log(newDeck1);
+    // console.log(card);
+    let points = parseInt(value);
+    if(value === "J" || value === "Q" || value === "K") points = 10;
+    if(value === "A") points = 11;
+    const card = [suit, value, points];
+    console.log(card);
   }
-  // console.log(card);
 }
